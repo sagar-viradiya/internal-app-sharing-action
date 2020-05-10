a09d289791b28e683e3e2090323d606fa1097162
Nemi Shah <nemishah1212@gmail.com>
2020-05-10 12:54:48 +0530

    Accept input parameters from github core and validate that all required inputs are present

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,45 @@
 import * as core from '@actions/core'
 import * as jwt from './jwt'
 
-function main(): String {
+const AuthServerUrlPropertyName = "auth_server";
+const AuthEmailPropertyName = "auth_email";
+const AuthKeyPropertyName = "auth_key";
+
+let authServerUrl: string | undefined;
+let authEmail: string | undefined;
+let authKey: string | undefined;
+
+function validateInput() {
+    let errorMessage: string = "";
+    if (!authServerUrl) {
+        errorMessage = `Missing ${AuthServerUrlPropertyName} in action inputs`;
+    } else if (!authEmail) {
+        errorMessage = `Missing ${AuthEmailPropertyName} in action inputs`;
+    } else if (!authKey) {
+        errorMessage = `Missing ${AuthKeyPropertyName} in action inputs`;
+    }
+
+    if (errorMessage) {
+        throw new Error(errorMessage);
+    }
+}
+
+function main(): string {
+    // Process and validate all input fields
+    authServerUrl = core.getInput(AuthServerUrlPropertyName);
+    authEmail = core.getInput(AuthEmailPropertyName);
+    authKey = core.getInput(AuthKeyPropertyName);
+    validateInput();
+
     //TODO : Call for jwt token
     //TODO : Call for access token
     //TODO : Call for upload
     return ""
 }
 
-core.setOutput("url", main())
+try {
+    const url = main();
+    core.setOutput("url", url)
+} catch (err) {
+    core.setFailed(err.message);
+}
