b73780aa2f7ad4eeee214ad5a11a136cc7e8f718
Nemi Shah <nemishah1212@gmail.com>
2020-05-16 15:03:32 +0530

    Add package name to inputs, Complete upload apk flow, Refactor main to accomodate for promises

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import * as core from '@actions/core'
 import * as jwt from './jwt'
+import { uploadFile } from "./upload";
 
 const AuthServerUrlPropertyName = "auth_server";
 const AuthEmailPropertyName = "auth_email";
 const AuthKeyPropertyName = "auth_key";
 const APKPathPropertyName = "path";
+const PackageNamePropertyName = "package_name";
 
 let authServerUrl: string | undefined;
 let authEmail: string | undefined;
 let authKey: string | undefined;
-let apkPath: String | undefined;
+let apkPath: string | undefined;
+let packageName: string | undefined;
 
 function validateInput() {
     let errorMessage: string = "";
@@ -21,6 +24,8 @@ function validateInput() {
         errorMessage = `Missing ${AuthKeyPropertyName} in action inputs`;
     } else if (!apkPath) {
         errorMessage = `Missing ${APKPathPropertyName} in action inputs`;
+    } else if (!packageName) {
+        errorMessage = `Missing ${PackageNamePropertyName} in action inputs`;
     }
 
     if (errorMessage) {
@@ -28,23 +33,25 @@ function validateInput() {
     }
 }
 
-function main(): string {
+async function main(): Promise<string> {
     // Process and validate all input fields
     authServerUrl = core.getInput(AuthServerUrlPropertyName);
     authEmail = core.getInput(AuthEmailPropertyName);
     authKey = core.getInput(AuthKeyPropertyName);
     apkPath = core.getInput(APKPathPropertyName);
+    packageName = core.getInput(PackageNamePropertyName);
     validateInput();
 
     //TODO : Call for jwt token
-    //TODO : Call for access token
-    //TODO : Call for upload
-    return ""
+    let accessToken: string = ""; //TODO : Call for access token
+    let url: string = await uploadFile(accessToken, apkPath, packageName);
+    return url;
 }
 
-try {
-    const url = main();
-    core.setOutput("url", url)
-} catch (err) {
-    core.setFailed(err.message);
-}
+main()
+    .then((url: string) => {
+        core.setOutput("url", url);
+    })
+    .catch((e) => {
+        core.setFailed(e.message);
+    });
--- src/upload.ts
+++ src/upload.ts
@@ -1,4 +1,25 @@
-export function uploadFile(accessToken: String): String {
-    // TODO : Upload file
-    return ""
+import * as fs from 'fs';
+
+// Type picked up from https://developers.google.com/android-publisher/api-ref/internalappsharingartifacts#resource-representations
+export type InternalAppSharingArtifact = {
+    downloadUrl: string,
+    certificateFingerprint: string,
+    sha256: string
+}
+
+export async function uploadFile(accessToken: String, path: string, packageName: string): Promise<string> {
+    const fileToUpload = fs.readFileSync(path, 'utf8')
+    const uploadURL: string = `https://www.googleapis.com/upload/androidpublisher/v3/applications/internalappsharing/${packageName}/artifacts/apk?uploadType=media`;
+    const uploadAPIResult = await fetch(uploadURL, {
+        headers: {
+            "Content-Type": "application/octet-stream",
+            "Authorization": `Bearer ${accessToken}`
+        },
+        method: "POST",
+        body: fileToUpload,
+    });
+
+    const uploadResult: InternalAppSharingArtifact = await (uploadAPIResult.json() as Promise<InternalAppSharingArtifact>);
+
+    return uploadResult.downloadUrl;
 }
